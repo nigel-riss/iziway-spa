44eab3484808083914060ea1b123166ecd4cc7c8
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-09-15 04:06:03 +0200

    Add mocks and App component

---
+++ src/components/app/app.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <h1>Hello App</h1>
+    )
+  }
+}
+
+
+export default App;
